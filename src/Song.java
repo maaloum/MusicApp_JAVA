7cdb90823e510a99216355d55de1e969d8711c6e
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-07-06 17:21:06 +0300

    Song Class

--- src/Song.java
+++ src/Song.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Song {
+public class Song {
+        String title;
+        String duration;
+        String genre;
+        String artist;
+
+
+    public Song(String title, String duration, String genre, String artist) {
+        this.title = title;
+        this.duration = duration;
+        this.genre = genre;
+        this.artist = artist;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
 }
